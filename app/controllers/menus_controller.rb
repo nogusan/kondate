85cbe6115f3e2f3cada534b5c53b0c0a8cfa9fd0
nogusan <standing.nogusan@gmail.com>
2023-02-20 20:47:57 +0900

    veiwの作成

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,10 +1,24 @@
 class MenusController < ApplicationController
+  before_action :move_to_signin
+
   def index
   end
 
   def new
+    @menu = Menu.new(menu_params)
   end
 
   def create
+  end
+
+  private
+  def move_to_signin
+    unless user_signed_in?
+      redirect_to new_user_registration
+    end
+  end
+
+  def menu_params
+    params.permit(:name, :calorie, :protein, :sugar, :lipid).merge(user_id: current_user.id)
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,10 @@
 class Menu < ApplicationRecord
+  belongs_to :user
+  has_many :schedules
+
+  validates :name, presence: true
+  validates :calorie, presence: true 
+  validates :protein, presence: true
+  validates :sugar, presence: true
+  validates :lipid, presence: true
 end
