3b40e15e35d31e7661f1c0ec9b709fdefae80b4c
nogusan <standing.nogusan@gmail.com>
2023-02-27 06:50:56 +0900

    アソシエーション設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,9 @@
 class Schedule < ApplicationRecord
   belongs_to :user
   belongs_to :menu
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre_id
+
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
 end
